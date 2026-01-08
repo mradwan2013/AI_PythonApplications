97f93748b593e7c7c1a21e4b53cefba8c846245c
Moataz Radwan <moataz_radwan_2013@hotmail.com>
2026-01-08 20:18:44 +0200

    apply hf classifier model

--- app.py
+++ app.py
@@ -3,17 +3,42 @@ from pydantic import BaseModel
 from transformers import pipeline
 
 # Create FastAPI app
-app = FastAPI(title="HuggingFace Translation Service")
-
-# Request model
-class TranslationRequest(BaseModel):
-    text: str
+app = FastAPI(title="Call HuggingFace Models")
 
 # Load Hugging Face model once (at startup)
 translator = pipeline("translation", model="Helsinki-NLP/opus-mt-ar-en")
 
-# POST endpoint to translate text
+classifier = pipeline("text-classification", model="abdulmatinomotoso/English_Grammar_Checker")
+
+class TextRequest(BaseModel):
+    text: str
+
+# POST Endpoint For text translation
 @app.post("/translate")
-async def translate(req: TranslationRequest):
+async def translate(req: TextRequest):
     result = translator(req.text)
     return {"translation": result[0]['translation_text']}
+
+
+# POST Endpoint For text classification
+@app.post("/classify")
+async def classify(req: TextRequest):
+
+    """ if u want to set the labels
+    label_map = {
+        "LABEL_0": "Correct Grammar",
+        "LABEL_1": "Grammar Error"
+    }
+    return {
+        "input_text": req.text,
+        "result": label_map.get(result["label"], "Unknown"),
+        "confidence": round(result["score"], 4)
+    }
+    """
+
+    result = classifier(req.text)
+    print(result)
+    return {
+        "label": result[0]["label"],
+        "score": result[0]["score"]
+    }
